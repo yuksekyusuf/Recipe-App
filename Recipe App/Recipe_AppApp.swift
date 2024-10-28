88f868a393a65262ada7143d7e18c45ba1f43d4c
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2024-10-27 22:12:07 -0400

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Ahmet Yusuf Yuksek on 27.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
